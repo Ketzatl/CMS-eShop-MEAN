f1cf0cce9c180a330f93e1d2b8a71bb7a965b620
Morgan <morgan.nott@hotmail.com>
2022-08-25 23:36:16 +0200

    feat/swagger: WIP Mise en place de Swagger et documentation de l'API

--- eshop-backend-nestjs/src/main.ts
+++ eshop-backend-nestjs/src/main.ts
@@ -1,10 +1,24 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const port = 3000;
     app.enableCors();
+    app.setGlobalPrefix('api');
+
+    const options = new DocumentBuilder()
+        .setTitle('eShop Momo API')
+        .setDescription('API NestJS exercice with fake eShop')
+        .setVersion('1.0.0')
+        .build();
+
+    // @ts-ignore
+    const document = SwaggerModule.createDocument(app, options);
+    // @ts-ignore
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(port);
 
     console.log("\x1b[33m%s\x1b[0m", "\n\t >>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<"); // yellow text
