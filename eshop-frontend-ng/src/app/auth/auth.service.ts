4e4d461cca1e7ff9206ec129338ce440f459e204
morgan.nott <morgan.nott@grtgaz.com>
2022-09-07 16:38:17 +0000

    feat/auth: Implémentation de la méthode POST pour la création d'un nouvel utilisateur

--- eshop-frontend-ng/src/app/auth/auth.service.ts
+++ eshop-frontend-ng/src/app/auth/auth.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private baseUrl = 'http://localhost:3000/users';
 
-  constructor() { }
+  constructor(private http: HttpClient  ) { }
+
+  register(credentials: any) {
+    const fullUrl = `${this.baseUrl}/register`;
+    this.http
+      .post<any>(fullUrl, credentials)
+      .subscribe(createdUser => {
+        console.log('createdUser : ', createdUser);
+      });
+  }
 }
