62c283c43961fe8b7ce19dd162679e3705546f4b
morgan.nott <morgan.nott@grtgaz.com>
2022-09-07 11:31:18 +0000

    feat/auth: Mise en place d'une Jwt Strategy

--- eshop-backend-nestjs/src/auth/auth.module.ts
+++ eshop-backend-nestjs/src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { AuthController } from './auth.controller';
 import {UsersModule} from "../users/users.module";
 import {PassportModule} from "@nestjs/passport";
 import {LocalStrategy} from "./local.strategy";
+import {JwtStrategy} from "./jwt.strategy";
 
 @Module({
   imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
---
+++ eshop-backend-nestjs/src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import {Injectable} from "@nestjs/common";
+import {PassportStrategy} from "@nestjs/passport";
+import {ExtractJwt} from "passport-jwt";
+import config from "../config";
+import {Strategy} from "passport-local";
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: config.secretKey,
+            });
+    }
+    async validate(payload: any) {
+        return {
+            userId: payload.sub,
+            username: payload.username,
+        };
+    };
+}
